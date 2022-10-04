ab0b138862bd430b38753580074df4aad42875d2
shafik720 <shafikrasel5@gmail.com>
2022-10-05 00:03:54 +0600

    sdfsdf

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ inputField = document.querySelector('.body-input input');
 
 inputField.addEventListener('keyup', e=>{
     if(e.key=='Enter' && inputField.value != ''){
-        console.log('ok');
+        
     }
 })
